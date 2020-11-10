f7da199cd4931f11b9cecf760006abe1dc01d408
saduquebe <saduquebe@unal.edu.co>
2020-11-09 22:10:10 -0500

    SD

--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 public class MyVisitor<T> extends BccLanguageBaseVisitor {
 
     HashMap<String, Object> table = new HashMap<>();
+    Boolean tk_break = false;
+    Boolean tk_next = false;
 
     @Override
-    public  T visitProg(BccLanguageParser.ProgContext ctx){
-        for(int i =0 ; i< ctx.fn_decl_list().size(); i++){
+    public T visitProg(BccLanguageParser.ProgContext ctx) {
+        for (int i = 0; i < ctx.fn_decl_list().size(); i++) {
             visitFn_decl_list(ctx.fn_decl_list(i));
         }
         return visitMain_prog(ctx.main_prog());
     }
+
     @Override
-    public T visitVar_decl(BccLanguageParser.Var_declContext ctx){
-        for(int i=0;i<ctx.ID().size();i++){
+    public T visitVar_decl(BccLanguageParser.Var_declContext ctx) {
+        for (int i = 0; i < ctx.ID().size(); i++) {
             String name = ctx.ID(i).getText();
-            if (table.get(name) != null){
+            if (table.get(name) != null) {
                 int line = ctx.ID(i).getSymbol().getLine();
                 int col = ctx.ID(i).getSymbol().getCharPositionInLine() + 1;
-                System.err.printf("<%d:%d> Error semántico, la variable con nombre: " +name + " ya ha sido declarada.",line,col);
+                System.err.printf("<%d:%d> Error semántico, la variable con nombre: " + name + " ya ha sido declarada.", line, col);
                 System.exit(-1);
             }else{
                 Datatype type = new Datatype();
@@ -38,11 +41,11 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
     }
 
     @Override
-    public T visitFn_decl_list(BccLanguageParser.Fn_decl_listContext ctx){
-        if (ctx.var_decl(0) != null){
+    public T visitFn_decl_list(BccLanguageParser.Fn_decl_listContext ctx) {
+        if (ctx.var_decl(0) != null) {
             visitVar_decl(ctx.var_decl(0));
         }
-        if(ctx.VAR() != null){
+        if (ctx.VAR() != null) {
             visitVar_decl(ctx.var_decl(1));
         }
         visitStmt_block(ctx.stmt_block());
@@ -51,97 +54,129 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
     }
 
     @Override
-    public T visitStmt_block(BccLanguageParser.Stmt_blockContext ctx){
+    public T visitStmt_block(BccLanguageParser.Stmt_blockContext ctx) {
         int stmtSize = ctx.stmt().size();
-        if(stmtSize > 1){
-            for (int i = 0; i < stmtSize; i++){
-                visitStmt(ctx.stmt(0));
+        if (stmtSize > 1) {
+            for (int i = 0; i < stmtSize; i++) {
+                visitStmt(ctx.stmt(i));
             }
-        }
-        else{
-            visitStmt(ctx.stmt(1));
+        } else {
+            visitStmt(ctx.stmt(0));
         }
         return null;
     }
 
     @Override
-    public T visitStmt(BccLanguageParser.StmtContext ctx){
-        if(ctx.PRINT() != null){
+    public T visitStmt(BccLanguageParser.StmtContext ctx) {
+        if (ctx.PRINT() != null) {
             System.out.println(visitLexpr(ctx.lexpr(0)));
-        }
-        else if (ctx.INPUT() != null){
+        } else if (ctx.INPUT() != null) {
             Scanner scan = new Scanner(System.in);
             String id = ctx.ID().getSymbol().getText();
-            System.out.println(id);
-            if(table.containsKey(id)) {
-                System.out.println("Se va a reemplazar el valor de: "+ id);
-                if((Double) table.get(id)== 0.0){
-                    table.replace(id,scan.nextDouble());
-                }
-                else{
-                    table.replace(id,scan.nextBoolean());
+            if (table.containsKey(id)) {
+                System.out.println("Se va a reemplazar el valor de: " + id);
+                if ((Double) table.get(id) == 0.0) {
+                    table.replace(id, scan.nextDouble());
+                } else {
+                    table.replace(id, scan.nextBoolean());
                 }
             }
-        }
-        else if(ctx.WHEN() != null){
-            //Hacer hasta que lexpr se de ?
-        }
-        else if(ctx.IF() != null){
-            if((Boolean) visitLexpr(ctx.lexpr(2))){
+        } else if (ctx.WHEN() != null) {
+            if (Utils.castToBoolean(visitLexpr(ctx.lexpr(0)))) {
+                visitStmt_block(ctx.stmt_block(0));
+            }
+        } else if (ctx.IF() != null) {
+            if (Utils.castToBoolean(visitLexpr(ctx.lexpr(0)))) {
+                visitStmt_block(ctx.stmt_block(0));
+            } else {
                 visitStmt_block(ctx.stmt_block(1));
             }
-            else{
-                visitStmt_block(ctx.stmt_block(2));
+        } else if (ctx.UNLESS() != null) {
+            if (!Utils.castToBoolean(visitLexpr(ctx.lexpr(0)))) {
+                visitStmt_block(ctx.stmt_block(0));
             }
-        }
-        else if(ctx.UNLESS() != null){
-            //se hace hasta que se cumpla la condicion?
-        }
-        else if(ctx.WHILE() != null){
-            while((Boolean) visitLexpr(ctx.lexpr(4))) {
-                visitStmt_block(ctx.stmt_block(4));
+        } else if (ctx.WHILE() != null) {
+            while (Utils.castToBoolean(visitLexpr(ctx.lexpr(0)))) {
+                if (!tk_next) {
+                    visitStmt_block(ctx.stmt_block(0));
+                } else {
+                    tk_next = false;
+                }
+                if (tk_break) {
+                    tk_break = false;
+                    break;
+                }
             }
-        }
-        else if(ctx.RETURN() != null){
-            return (T) visitLexpr(ctx.lexpr(5));
-        }
-        else if(ctx.UNTIL() != null){
-            while(!(Boolean) visitLexpr(ctx.lexpr(6))){
-                visitStmt_block(ctx.stmt_block(6));
+        } else if (ctx.RETURN() != null) {
+            return (T) visitLexpr(ctx.lexpr(0));
+        } else if (ctx.UNTIL() != null) {
+            while (!Utils.castToBoolean(visitLexpr(ctx.lexpr(0)))) {
+                if (!tk_next) {
+                    visitStmt_block(ctx.stmt_block(0));
+                } else {
+                    tk_next = false;
+                }
+                if (tk_break) {
+                    break;
+                }
             }
-        }
-        else if(ctx.LOOP() != null){
+        } else if (ctx.LOOP() != null) {
             //loop infinito????
-        }
-        else if(ctx.DO() != null){
-            if(ctx.WHILE() != null){
-                do{
-                    visitStmt_block(ctx.stmt_block(7));
-                }while((Boolean) visitLexpr(ctx.lexpr(7)));
+        } else if (ctx.DO() != null) {
+            if (ctx.WHILE() != null) {
+                do {
+                    if (!tk_next) {
+                        visitStmt_block(ctx.stmt_block(0));
+                    } else {
+                        tk_next = false;
+                    }
+                    if (tk_break) {
+                        tk_break = false;
+                        break;
+                    }
+                } while ((Boolean)visitLexpr(ctx.lexpr(0)));
+            } else if (ctx.UNTIL() != null) {
+                do {
+                    if (!tk_next) {
+                        visitStmt_block(ctx.stmt_block(0));
+                    } else {
+                        tk_next = false;
+                    }
+                } while (!Utils.castToBoolean(visitLexpr(ctx.lexpr(0))));
             }
-            else if(ctx.UNTIL() != null){
-                do{
-                    visitStmt_block(ctx.stmt_block(8));
-                }while(!(Boolean) visitLexpr(ctx.lexpr(8)));
-            }
-        }
-        else if(ctx.REPEAT() != null){
+        } else if (ctx.REPEAT() != null) {
             //repetir un bloque un número de veces?
             int iterator = Integer.parseInt(ctx.NUM().getSymbol().getText());
-            for (int i = 0; i < iterator; i++){
-                visitStmt_block(ctx.stmt_block(9));
+            for (int i = 0; i < iterator; i++) {
+                if (!tk_next) {
+                    visitStmt_block(ctx.stmt_block(0));
+                } else {
+                    tk_next = false;
+                }
+                if (tk_break) {
+                    tk_break = false;
+                    break;
+                }
+
             }
         }
         else if(ctx.FOR() != null){
-            for (visitLexpr(ctx.lexpr(9)); (Boolean)  visitLexpr(ctx.lexpr(10)); visitLexpr(ctx.lexpr(11))){ //?
-                visitStmt_block(ctx.stmt_block(10));
+            for (visitLexpr(ctx.lexpr(0)); Utils.castToBoolean(visitLexpr(ctx.lexpr(1))); visitLexpr(ctx.lexpr(2))){ //?
+                if (!tk_next) {
+                    visitStmt_block(ctx.stmt_block(0));
+                } else {
+                    tk_next = false;
+                }
+                if (tk_break) {
+                    tk_break = false;
+                    break;
+                }
             }
         }
-        else if(ctx.NEXT() != null){
-            //No entiendo
-        }
-        else if(ctx.BREAK() != null){
-            //No entiendo
+        else if (ctx.NEXT() != null) {
+            tk_next = true;
+        } else if (ctx.BREAK() != null) {
+            tk_break = true;
         }
         else if(ctx.ASSIGNOP() != null){
             T result = visitLexpr(ctx.lexpr(0));
@@ -193,10 +228,10 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
             }
         }
         else if(ctx.SUBS() != null){
-            //FALTA
+            //Diferencia con ++i?
         }
         else if(ctx.ADD() != null){
-            //FALTA
+            //Diferencias con --i?
         }
         return null;
     }
@@ -210,7 +245,7 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
         boolean result = Utils.castToBoolean(visitNexpr(ctx.nexpr(0)));
 
         if (ctx.AND().size() != 0) {
-            for (int i = 1; i < ctx.AND().size(); i++){
+            for (int i = 0; i < ctx.AND().size(); i++){
                 boolean nexpr = Utils.castToBoolean(visitNexpr(ctx.nexpr(i + 1)));
                 result = result && nexpr;
             }
@@ -218,7 +253,7 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
             return (T) (Boolean) result;
         }
 
-        for (int i = 1; i < ctx.OR().size(); i++){
+        for (int i = 0; i < ctx.OR().size(); i++){
             boolean nexpr = Utils.castToBoolean(visitNexpr(ctx.nexpr(i + 1)));
             result = result || nexpr;
         }
@@ -272,7 +307,7 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
         for (int i = 1 ; i < ctx.term().size() ; i++){
             Double term = Utils.castToDouble(visitTerm(ctx.term(i)));
             String op = ctx.SUMOP(i - 1).getText();
-            switch(op){
+            switch (op) {
                 case "+":
                     total += term;
                     break;
@@ -285,8 +320,8 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
     }
 
     @Override
-    public T visitTerm(BccLanguageParser.TermContext ctx){
-        if (ctx.factor().size() ==1) return visitFactor(ctx.factor(0));
+    public T visitTerm(BccLanguageParser.TermContext ctx) {
+        if (ctx.factor().size() == 1) return visitFactor(ctx.factor(0));
 
         Double answer = Utils.castToDouble(visitFactor(ctx.factor(0)));
         for(int i =1;i<ctx.factor().size();i++){
@@ -303,24 +338,24 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
                         answer %= fi;
                         break;
                 }
-            } catch (Exception e){ // Error: Uno de los factores(Bool o ID) no es numero
+            } catch (Exception e) { // Error: Uno de los factores(Bool o ID) no es numero
                 int line = ctx.factor(i).NUM().getSymbol().getLine();
                 int col = ctx.factor(i).NUM().getSymbol().getCharPositionInLine() + 1;
-                if (ctx.factor(i).BOOL() != null){
+                if (ctx.factor(i).BOOL() != null) {
                     String symbol = ctx.factor(i).BOOL().getSymbol().getText();
-                    System.err.printf("<%d:%d> Error semántico, " +symbol + " no es un símbolo válido para una para la operación.",line,col);
-                }else{
+                    System.err.printf("<%d:%d> Error semántico, " + symbol + " no es un símbolo válido para una para la operación.", line, col);
+                } else {
                     String varName = ctx.factor(i).ID().getSymbol().getText();
-                    System.err.printf("<%d:%d> Error semántico, el valor la variable " +varName + " no es válido para la operación.",line,col);
+                    System.err.printf("<%d:%d> Error semántico, el valor la variable " + varName + " no es válido para la operación.", line, col);
                 }
                 System.exit(-1);
             }
         }
-        return (T)answer;
+        return (T) answer;
     }
 
     @Override
-    public T visitFactor(BccLanguageParser.FactorContext ctx){
+    public T visitFactor(BccLanguageParser.FactorContext ctx) {
         if (ctx.NUM() != null) {
             return (T) (Double) Double.parseDouble(ctx.NUM().getText());
         }
@@ -329,10 +364,10 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
         }
         if (ctx.RIGHT_INC != null || ctx.RIGHT_DEC != null || ctx.LEFT_INC != null || ctx.LEFT_DEC != null) {
             String name = ctx.ID().getText();
-            if(table.get(name) == null){
+            if (table.get(name) == null) {
                 int line = ctx.ID().getSymbol().getLine();
-                int col = ctx.ID().getSymbol().getCharPositionInLine()+1;
-                System.err.printf("<%d:%d> Error semantico, la variable con nombre: \"" + name + "\" no ha sido declarada.\n",line,col);
+                int col = ctx.ID().getSymbol().getCharPositionInLine() + 1;
+                System.err.printf("<%d:%d> Error semantico, la variable con nombre: \"" + name + "\" no ha sido declarada.\n", line, col);
                 System.exit(-1);
             }
             Datatype var = (Datatype) table.get(name);
@@ -350,10 +385,10 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
         }
         if (ctx.ID() != null) {
             String name = ctx.ID().getText();
-            if (table.get(name) == null){
+            if (table.get(name) == null) {
                 int line = ctx.ID().getSymbol().getLine();
-                int col = ctx.ID().getSymbol().getCharPositionInLine()+1;
-                System.err.printf("<%d:%d> Error semantico, la variable con nombre: \"" + name + "\" no ha sido declarada.\n",line,col);
+                int col = ctx.ID().getSymbol().getCharPositionInLine() + 1;
+                System.err.printf("<%d:%d> Error semantico, la variable con nombre: \"" + name + "\" no ha sido declarada.\n", line, col);
                 System.exit(-1);
             }
 
@@ -370,12 +405,12 @@ public class MyVisitor<T> extends BccLanguageBaseVisitor {
     }
 
     @Override
-    public T visitMain_prog(BccLanguageParser.Main_progContext ctx){
-        if(ctx.VAR() != null){
+    public T visitMain_prog(BccLanguageParser.Main_progContext ctx) {
+        if (ctx.VAR() != null) {
             visitVar_decl(ctx.var_decl());
         }
-        if(ctx.stmt() != null){
-            for(int i=0;i< ctx.stmt().size();i++){
+        if (ctx.stmt() != null) {
+            for (int i = 0; i < ctx.stmt().size(); i++) {
                 visitStmt(ctx.stmt(i));
             }
         }
